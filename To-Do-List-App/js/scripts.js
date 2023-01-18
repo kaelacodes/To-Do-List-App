16f305b36db3432874a1d58506fc6a3fabd9cf7d
Kaela Pardo <120235236+kaelacodes@users.noreply.github.com>
2023-01-17 16:03:00 -0800

    Add files via upload

---
+++ To-Do-List-App/js/scripts.js
@@ -0,0 +1,35 @@
+function newItem(){
+
+//1. Adding a new item to othe list of items:
+
+let li = $('<li></li>');
+let inputValue = $('#input').val();
+li.append(inputValue);
+
+if (inputValue === '') {
+    alert("You must write something!");
+}else {
+    $('#list').append(li);
+}
+
+//2. Crossing out an item from the list of items:
+
+li.on("dblclick", crossOut);
+function crossOut() {
+    li.toggleClass("strike");
+}
+
+//3. Adding a delete button "X":
+
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append(document.createTextNode('X'));
+li.append(crossOutButton)
+
+crossOutButton.on('click', deleteListItem);
+function deleteListItem(){
+    li.addClass('delete')
+}
+
+//4. Reordering the items:
+$('#list').sortable();
+}
